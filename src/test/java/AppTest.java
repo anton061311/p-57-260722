199ac50353c0ce7b06fec4335f3a18ed5da8bcc0
anton <anton061311@gmail.com>
2026-07-22 16:05:17 +0900

    feat: AppTest, App구현

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,35 @@
+import com.back.App;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+
+    @Test
+    @DisplayName("1 + 2 = 3")
+    void t1(){
+        App app = new App();
+        int result = app.plus(1, 2); // 3이 나오길 바래
+
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("10 + 20 = 30")
+    void t2() {
+        App app = new App();
+        int rst = app.plus(10, 20);
+
+        assertThat(rst).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("0 + 5 = 5")
+    void t3() {
+        App app = new App();
+        int rst = app.plus(0, 5);
+
+        assertThat(rst).isEqualTo(5);
+    }
+}
